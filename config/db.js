becacdb51297f914412c96c4a3944d6cadfe2959
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-17 19:03:21 +0530

    Added: Config for connecting mongodb

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		if (process.env.ENV === "development") {
+			console.log(`MonogoDB Connected: ${conn.connection.host}`);
+		}
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
