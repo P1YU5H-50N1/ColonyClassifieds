5398ea25fb908163f40124f0fd6cdffeadec2853
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-17 19:13:53 +0530

    Corrected: ENV variable to MODE

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
-		if (process.env.ENV === "development") {
+		if (process.env.MODE === "development") {
 			console.log(`MonogoDB Connected: ${conn.connection.host}`);
 		}
 	} catch (err) {
