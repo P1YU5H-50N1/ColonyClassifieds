a8ae03b439d13bd811700ae92c2710e60ddf398b
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-17 19:55:57 +0530

    Implemented: User Model

---
+++ auth/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const pointSchema = require("./pointModel")
+
+const UserSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please add a name"],
+		},
+		email: {
+			type: String,
+			required: [true, "Please add an email"],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Please add a password"],
+		},
+		location: {
+			type: pointSchema,
+            required: [true, "Add a location"]
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("User", UserSchema);
