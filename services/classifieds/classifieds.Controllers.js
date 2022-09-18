0683c8da53d98e83284eeb81cd30da1653437581
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 23:12:35 +0530

    Implemented getclassifieds controller & resolved bug in extended reference pattern

--- services/classifieds/classifieds.Controllers.js
+++ services/classifieds/classifieds.Controllers.js
@@ -3,9 +3,16 @@ const Classified = require("../../models/classifiedModel");
 const Audience = require("../../models/audienceModel");
 const User = require("../../models/userModel");
 
-const getClassifieds = asyncHandler((req, res) => {
+const getClassifieds = asyncHandler(async (req, res) => {
+	const classifieds = await Classified.find({ audience: req.user });
+	const audience_classifieds = await Audience.find(
+		{ user: req.user },
+		"classified"
+	).populate("classified");
 	res.status(200).json({
 		message: "get Classifieds",
+		classifieds,
+		audience_classifieds,
 	});
 });
 
@@ -31,7 +38,7 @@ const postClassified = asyncHandler(async (req, res) => {
 	const audience = await (
 		await User.find(geoQuery).limit(50)
 	).filter((user) => !user.equals(req.user));
-	
+
 	const more_audience = await User.find(geoQuery).skip(50); //this can be a batch job while scaling
 
 	const classified = await Classified.create({
@@ -45,10 +52,12 @@ const postClassified = asyncHandler(async (req, res) => {
 	});
 
 	if (more_audience.length !== 0) {
-		const extended_audience = more_audience.map((user) => {
-			user, classified;
-		});
-		await Audience.insertMany(extended_audience);
+		const extended_audience = more_audience.map((user) => ({
+			user,
+			classified,
+		}));
+		const audiences = await Audience.insertMany(extended_audience);
+		console.log(audiences);
 	}
 
 	res.status(200).json({
