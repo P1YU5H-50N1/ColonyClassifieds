5dfd8154997ba65288ffff44996b3fb4c8d1f273
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 13:01:28 +0530

    Implemented: validation schema for /register route

---
+++ services/auth/validators/auth.validatorSchema.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+	email: Joi.string().email({
+		minDomainSegments: 2,
+		tlds: { allow: ["com", "net"] },
+	}).required(),
+
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+
+	name: Joi.string().min(3).max(30).required(),
+
+	location: Joi.object()
+		.keys({
+			lat: Joi.number(),
+			long: Joi.number(),
+		})
+		.and("lat", "long").required(),
+});
+
+module.exports = { registerSchema };
