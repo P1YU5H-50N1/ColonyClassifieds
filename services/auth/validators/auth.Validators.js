d77b86c769a6e9b38fc9dbd27dd2b276eaad55ee
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 15:19:40 +0530

    Implemented: Validator for login Route

--- services/auth/validators/auth.Validators.js
+++ services/auth/validators/auth.Validators.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const { registerSchema } = require("./auth.validatorSchema");
+const { registerSchema,loginSchema } = require("./auth.validatorSchema");
 
 const registerValidator = (req, res, next) => {
 	const payload = {
@@ -19,4 +19,19 @@ const registerValidator = (req, res, next) => {
 	}
 };
 
-module.exports = { registerValidator };
+const loginValidator = (req, res, next) => {
+	const payload = {
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+	const { error } = loginSchema.validate(payload);
+	if (error) {
+		res.status(406);
+		throw new Error(error.details[0].message);
+	} else {
+		next();
+	}
+};
+
+module.exports = { registerValidator,loginValidator };
