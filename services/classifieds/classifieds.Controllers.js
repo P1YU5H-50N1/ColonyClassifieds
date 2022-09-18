5525ea21025a0235337a09d623b4bea4346bbe9a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 20:48:02 +0530

    Added: geoQuery for more audience for post classified controller

--- services/classifieds/classifieds.Controllers.js
+++ services/classifieds/classifieds.Controllers.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Classified = require("../../models/classifiedModel");
+const Audience = require("../../models/audienceModel");
+const User = require("../../models/userModel");
 
 const getClassifieds = asyncHandler((req, res) => {
 	res.status(200).json({
@@ -15,14 +17,36 @@ const editClassified = asyncHandler((req, res) => {
 
 const postClassified = asyncHandler(async (req, res) => {
 	const { title, description, broadcast_radius } = req.body;
-	const  location  = req.body.location || req.user.location;
+	const location = req.user.location;
+
+	const geoQuery = {
+		location: {
+			$near: {
+				$geometry: location,
+				$maxDistance: broadcast_radius * 1000,
+			},
+		},
+	};
+
+	const audience = await User.find(geoQuery).limit(50);
+	const more_audience = await User.find(geoQuery).skip(50); //this can be a batch job while scaling
+
 	const classified = await Classified.create({
 		title,
 		description,
 		broadcast_radius,
+		audience,
+		more_audience: more_audience.length !== 0,
 		owner: req.user,
 		location,
 	});
+
+	if (more_audience.length !== 0) {
+		const extended_audience = more_audience.map((user) => {
+			user, classified;
+		});
+		await Audience.insertMany(extended_audience)
+	}
 
 	res.status(200).json({
 		message: "post Classified",
