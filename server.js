1c1dd434c4aae83603cc385a9681f26be082fcf2
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 10:08:58 +0530

    Added: SetNotifierKeys & notifier routes

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const setNotifierKeys = require("./config/notifier");
 const { errorHandler } = require("./middlewares/errorMiddlerware");
 const authRouter = require("./services/auth/auth.Routes");
 const classifiedsRouter = require("./services/classifieds/classifieds.Routes");
 const biddingRouter = require("./services/bidding/bidding.Routes");
+const notifierRouter = require("./services/notifier/notifier.Routes");
 
 connectDB();
+setNotifierKeys();
 
 const app = express();
 
@@ -20,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", authRouter);
 app.use("/api/classified", classifiedsRouter);
 app.use("/api/bid", biddingRouter);
+app.use("/api/notifier", notifierRouter);
 
 app.use(errorHandler);
 
