7e4015dd3ead787d0f9526271c11f210dd5c958f
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-19 00:06:08 +0530

    Implemented: PostBid controller

--- services/bidding/bidding.Controllers.js
+++ services/bidding/bidding.Controllers.js
@@ -1,18 +1,38 @@
 const asyncHandler = require("express-async-handler");
+const Bid = require("../../models/bidModel");
+const Classified = require("../../models/classifiedModel");
 
 const acceptBid = asyncHandler((req, res) => {
 	res.status(200).json({
 		message: "Accept Bid for a classified and move to archive",
 	});
 });
-const allBids = asyncHandler((req, res) => {
+const allBids = asyncHandler(async (req, res) => {
+	
 	res.status(200).json({
 		message: "All Bids for a classified",
 	});
 });
-const postBid = asyncHandler((req, res) => {
+
+const postBid = asyncHandler(async (req, res) => {
+	
+	const { price } = req.body;
+	const { user } = req;
+	const classified = await Classified.findOne({ _id: req.body.classified });
+	
+	if (!classified) {
+		throw new Error("Classified doesn't exists");
+	}
+
+	const bid = await Bid.create({ classified, user, price });
+
+	if (!bid) {
+		throw new Error("Invalid params");
+	}
+
 	res.status(200).json({
 		message: "post Bid",
+		bid,
 	});
 });
 
