a59ec94a3721b832cb3207994713e7e9e4dc1d6a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 22:47:40 +0530

    Resolved Edge case: constraints on latitude and longitude

--- services/auth/validators/auth.validatorSchema.js
+++ services/auth/validators/auth.validatorSchema.js
@@ -16,8 +16,8 @@ const registerSchema = Joi.object({
 
 	location: Joi.object()
 		.keys({
-			lat: Joi.number(),
-			long: Joi.number(),
+			lat: Joi.number().min(-90).max(90).required(),
+			long: Joi.number().min(-180).max(180).required(),
 		})
 		.and("lat", "long")
 		.required(),
