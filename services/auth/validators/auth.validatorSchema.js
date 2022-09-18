f2f7f947be369e317cb1466e4f4836bbc9cf91d0
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 15:19:11 +0530

    Implemented: Validation schema for login route

--- services/auth/validators/auth.validatorSchema.js
+++ services/auth/validators/auth.validatorSchema.js
@@ -1,12 +1,16 @@
 const Joi = require("joi");
 
 const registerSchema = Joi.object({
-	email: Joi.string().email({
-		minDomainSegments: 2,
-		tlds: { allow: ["com", "net"] },
-	}).required(),
+	email: Joi.string()
+		.email({
+			minDomainSegments: 2,
+			tlds: { allow: ["com", "net"] },
+		})
+		.required(),
 
-	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+	password: Joi.string()
+		.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+		.required(),
 
 	name: Joi.string().min(3).max(30).required(),
 
@@ -15,7 +19,21 @@ const registerSchema = Joi.object({
 			lat: Joi.number(),
 			long: Joi.number(),
 		})
-		.and("lat", "long").required(),
+		.and("lat", "long")
+		.required(),
 });
 
-module.exports = { registerSchema };
+const loginSchema = Joi.object({
+	email: Joi.string()
+		.email({
+			minDomainSegments: 2,
+			tlds: { allow: ["com", "net"] },
+		})
+		.required(),
+
+	password: Joi.string()
+		.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+		.required(),
+});
+
+module.exports = { registerSchema,loginSchema };
