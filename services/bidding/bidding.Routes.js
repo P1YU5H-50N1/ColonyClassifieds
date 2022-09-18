eb73816e21a19b7ca326b4e33eeb20408d266d8b
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-19 00:05:35 +0530

    Added: postBid Validator

--- services/bidding/bidding.Routes.js
+++ services/bidding/bidding.Routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { postBidValidator } = require("./validators/bidding.Validators");
 const { acceptBid, allBids, postBid } = require("./bidding.Controllers");
 
-router.route("/").get(allBids).post(postBid).put(acceptBid);
+router.route("/").get(allBids).post(postBidValidator, postBid).put(acceptBid);
 
 module.exports = router;
