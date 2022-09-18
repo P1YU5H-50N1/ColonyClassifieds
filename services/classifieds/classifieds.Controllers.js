f3f440163444de3af968c2b962cae9a0b7062d54
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 19:39:27 +0530

    Implemented: Post Classified controller

--- services/classifieds/classifieds.Controllers.js
+++ services/classifieds/classifieds.Controllers.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const Classified = require("../../models/classifiedModel");
 
 const getClassifieds = asyncHandler((req, res) => {
 	res.status(200).json({
@@ -12,9 +13,20 @@ const editClassified = asyncHandler((req, res) => {
 	});
 });
 
-const postClassified = asyncHandler((req, res) => {
+const postClassified = asyncHandler(async (req, res) => {
+	const { title, description, broadcast_radius } = req.body;
+	const  location  = req.body.location || req.user.location;
+	const classified = await Classified.create({
+		title,
+		description,
+		broadcast_radius,
+		owner: req.user,
+		location,
+	});
+
 	res.status(200).json({
 		message: "post Classified",
+		ad: classified,
 	});
 });
 
