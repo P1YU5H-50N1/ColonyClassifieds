4ff60aaa89da0101f13139db93c54d85cb88f3d5
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-18 13:03:24 +0530

    Added: Validator to /register

--- services/auth/auth.Routes.js
+++ services/auth/auth.Routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("./auth.Controllers");
+const {registerValidator} = require("./validators/auth.Validators")
 
-router.post("/register", register);
+router.post("/register",registerValidator, register);
 
 router.post("/login", login);
 
