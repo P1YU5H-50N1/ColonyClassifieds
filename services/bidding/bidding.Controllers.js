a6a9e47a4acfe9b1ac6db883542996788a2fe923
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-19 08:18:26 +0530

    Implemented: Controller for getting all bids for a classified

--- services/bidding/bidding.Controllers.js
+++ services/bidding/bidding.Controllers.js
@@ -8,18 +8,28 @@ const acceptBid = asyncHandler((req, res) => {
 	});
 });
 const allBids = asyncHandler(async (req, res) => {
-	
+	const { classified_id } = req.params;
+	const classified = await Classified.findOne({_id: classified_id})
+	if(!classified){
+		req.status(400)
+		throw new Error("Classified doesn't exist")
+	}
+	if(!classified.owner._id.equals(req.user._id)){
+		res.status(401)
+		throw new Error("Unauthorized")
+	}
+	const bids = await Bid.find({ classified });
 	res.status(200).json({
 		message: "All Bids for a classified",
+		bids: [...bids,...classified.bids]
 	});
 });
 
 const postBid = asyncHandler(async (req, res) => {
-	
 	const { price } = req.body;
 	const { user } = req;
 	const classified = await Classified.findOne({ _id: req.body.classified });
-	
+
 	if (!classified) {
 		throw new Error("Classified doesn't exists");
 	}
