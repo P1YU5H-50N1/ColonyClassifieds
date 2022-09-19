e153557fe79aa0b1995c90d424de5d92cf2c4875
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-19 08:15:51 +0530

    Implemented: Request Validator for get classified

--- services/bidding/validators/bidding.ValidatorSchema.js
+++ services/bidding/validators/bidding.ValidatorSchema.js
@@ -4,5 +4,8 @@ const postBidSchema = Joi.object({
 	price: Joi.number().min(1).required(),
     classified: Joi.string().hex().length(24).required()
 });
+const getBidSchema = Joi.object({
+    classified_id: Joi.string().hex().length(24).required()
+})
 
-module.exports = { postBidSchema };
+module.exports = { postBidSchema,getBidSchema };
--- services/bidding/validators/bidding.Validators.js
+++ services/bidding/validators/bidding.Validators.js
@@ -1,18 +1,29 @@
-const {postBidSchema}  = require("./bidding.ValidatorSchema")
+const { postBidSchema,getBidSchema } = require("./bidding.ValidatorSchema");
 
-const postBidValidator = (req,res,next) =>{
-    const payload = {
-        price : req.body.price,
-        classified: req.body.classified
-    }
-    const { error } = postBidSchema.validate(payload);
+const postBidValidator = (req, res, next) => {
+	const payload = {
+		price: req.body.price,
+		classified: req.body.classified,
+	};
+	const { error } = postBidSchema.validate(payload);
 	if (error) {
 		res.status(406);
 		throw new Error(error.details[0].message);
 	} else {
 		next();
 	}
+};
 
-}
-
-module.exports = {postBidValidator}
+const getBidsValidator = (req, res, next) => {
+	const payload = {
+		classified_id:req.params.classified_id
+	}
+	const { error } = getBidSchema.validate(payload);
+	if (error) {
+		res.status(406);
+		throw new Error(error.details[0].message);
+	} else {
+		next();
+	}
+};
+module.exports = { postBidValidator, getBidsValidator };
